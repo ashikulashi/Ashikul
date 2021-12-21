7e98c614ab2cc851cb676e7926189980eb7d881c
tharik9789 <tharik9789@gmail.com>
2021-12-21 13:08:10 +0530

    bala finished his coding

--- src/org/sample/Sample.java
+++ src/org/sample/Sample.java
@@ -2,6 +2,11 @@ package org.sample;
 
 public class Sample {
 	
+	private void code2() {
+		System.out.println("code2");
+
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Ashik");
 	}
